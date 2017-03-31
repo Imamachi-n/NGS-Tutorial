06c309c4ade651d98598aa6a21217d51d1e9add7
imamachi-n <imamachigatugatu@hotmail.co.jp>
2017-03-31 11:39:01 +0900

    no message

--- Ribo-seq_RiboTaper/Ribo-seq-RiboTaper-3-define_ORF_STEP3.sh
+++ Ribo-seq_RiboTaper/Ribo-seq-RiboTaper-3-define_ORF_STEP3.sh
@@ -5,8 +5,18 @@
 #$ -l s_vmem=60G
 #$ -l mem_req=60G
 
-rnaseq="/home/akimitsu/data/Ribo-seq_HEK293_Gao_2015/HEK293_RNA-seq/STAR_output_SRR1630838_HEK293_RNA-seq/STAR_accepted_hits_genome.bam"
-riboseq="/home/akimitsu/data/Ribo-seq_HEK293_Gao_2015/HEK293_Ribo-seq/STAR_output_SRR1630831_HEK293_Ribo-seq/STAR_accepted_hits_genome.bam"
+source activate ribotaper
+
+RNA_bamfile="/home/akimitsu/data/NGS_tutorial/Ribo-seq/STAR_output_RNA-Seq_HEK293_Cell_SRR1630838/RNA-Seq_HEK293_Cell_SRR1630838_4_STAR_result_Aligned.sortedByCoord.out.bam"
+Ribo_bamfile="/home/akimitsu/data/NGS_tutorial/Ribo-seq/STAR_output_Ribo-Seq_HEK293_Cell_Control_SRR1630831/Ribo-Seq_HEK293_Cell_Control_SRR1630831_4_STAR_result_Aligned.sortedByCoord.out.bam"
+annotation_dir="/home/akimitsu/software/RiboTaper_v1.3/annotation/hg38_Gencode_v24"
+read_lenghts_ribo="27,28,29"
+cutoffs="12,12,12"
+scripts_dir="/home/akimitsu/software/RiboTaper_v1.3/scripts"
+bedtools_dir="/home/akimitsu/software/RiboTaper_v1.3/bedtools_dir"
+n_cores="16"
 
 echo "-- Starting RiboTaper..."
-Ribotaper_step3.sh ${riboseq} ${rnaseq} /home/akimitsu/software/RiboTaper_v1.3/annotation/hg38_Gencode_v24 27,28,29 12,12,12 /home/akimitsu/software/RiboTaper_v1.3/scripts /home/akimitsu/software/RiboTaper_v1.3/bedtools_dir 16
+Ribotaper_step3.sh ${Ribo_bamfile} ${RNA_bamfile} \
+${annotation_dir} ${read_lenghts_ribo} ${cutoffs} \
+${scripts_dir} ${bedtools_dir} ${n_cores}
